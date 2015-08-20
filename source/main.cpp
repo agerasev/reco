462601ebeed4047f3f9c3029f9801f3ccb4fd93c
Alexey Gerasev <alexey.gerasev@gmail.com>
2015-08-20 15:54:19 +0600

    backpropagation test

--- source/main.cpp
+++ source/main.cpp
@@ -1,47 +1,146 @@
 #include <iostream>
 #include <string>
-#include <nn/net.hpp>
-#include <nn/sw/layerext.hpp>
-#include <nn/sw/conn.hpp>
+#include <nn/bp/net.hpp>
+#include <nn/sw/bp/layerext.hpp>
+#include <nn/sw/bp/conn.hpp>
+
+static unsigned seed = 0x378fea63;
+static const unsigned lcaa = 0xa63b12ef;
+static const unsigned lcab = 0x3724fcef;
+
+unsigned random()
+{
+	return seed = lcaa*seed + lcab;
+}
+
+float urandom01()
+{
+	return float(random())/0xffffffff;
+}
+
+float urandom11()
+{
+	return 2.0f*urandom01() - 1.0f;
+}
+
+std::ostream &operator << (std::ostream &os, const Layer::Buffer &buffer)
+{
+	int size = buffer.getSize();
+	float *data = new float[size];
+	buffer.read(data);
+	os << buffer.isValid() << ":  ";
+	for(int i = 0; i < size; ++i)
+		os << data[i] << ' ';
+	delete[] data;
+	return os;
+}
+
+void printStep(int i, const Layer_BP *in, const Layer_BP *out)
+{
+	std::cout << "step " << i << ':' << std::endl;
+	std::cout << in->getInput() << '\t' << in->getOutputError() << std::endl;
+	std::cout << in->getOutput() << '\t' << in->getInputError() << std::endl;
+	std::cout << out->getInput() << '\t' << out->getOutputError() << std::endl;
+	std::cout << out->getOutput() << '\t' << out->getInputError() << std::endl;
+}
+
+void printConn(const Conn_BP *conn)
+{
+	int sx = conn->getInputSize(), sy = conn->getOutputSize();
+	float *data = new float[2*(sx + 1)*sy];
+	conn->getWeight().read(data);
+	conn->getBias().read(data + sx*sy);
+	conn->getWeightGrad().read(data + (sx + 1)*sy);
+	conn->getBiasGrad().read(data + (sx + 1)*sy + sx*sy);
+	for(int iy = 0; iy < sy; ++iy)
+	{
+		for(int ix = 0; ix < sx; ++ix)
+		{
+			std::cout << data[iy*sx + ix] << ' ';
+		}
+		std::cout << ' ' << data[sx*sy + iy] << '\t';
+		for(int ix = 0; ix < sx; ++ix)
+		{
+			std::cout << data[(sx + 1)*sy + iy*sx + ix] << ' ';
+		}
+		std::cout << ' ' << data[(sx + 1)*sy + sx*sy + iy] << std::endl;
+	}
+	delete[] data;
+}
 
 int main(int argc, char *argv[])
 {
-	Net net;
+	Net_BP net;
 	
 	const Layer::ID in_id = 1, out_id = 2;
 	const Conn::ID conn_id = 1;
-	const int in_size = 4, out_size = 4;
+	static const int in_size = 4, out_size = 4;
 	
-	Layer *in;
-	Layer *out;
-	Conn *conn;
+	Layer_BP *in;
+	Layer_BP *out;
+	Conn_BP *conn;
 	
-	in = new LayerSW(in_id, in_size);
-	out = new LayerExtSW<EXT_SIGMOID>(out_id, out_size);
-	conn = new ConnSW(conn_id, in_size, out_size);
+	in = new LayerSW_BP(in_id, in_size);
+	out = new LayerExtSW_BP<EXT_SIGMOID>(out_id, out_size);
+	conn = new ConnSW_BP(conn_id, in_size, out_size);
 	
 	net.addLayer(in);
 	net.addLayer(out);
 	net.addConn(conn, in->getID(), out->getID());
 	
-	float in_data[4] = {1,2,3,4};
-	float weight_data[16] = {1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1};
-	float bias_data[4] = {1,1,1,1};
+	float weight_data[in_size*out_size];
+	for(int i = 0; i < in_size*out_size; ++i)
+	{
+		weight_data[i] = urandom11();
+	}
+	float bias_data[out_size];
+	for(int i = 0; i < out_size; ++i)
+	{
+		weight_data[i] = urandom11();
+	}
 	
-	in->getInput().write(in_data);
-	conn->writeWeight(weight_data);
-	conn->writeBias(bias_data);
+	conn->getWeight().write(weight_data);
+	conn->getBias().write(bias_data);
 	
-	for(int i = 0; i < 2; ++i)
-		net.stepForward();
-	
-	float out_data[4];
-	
-	out->getOutput().read(out_data);
-	
-	for(int i = 0; i < 4; ++i)
-		std::cout << out_data[i] << ' ';
-	std::cout << std::endl;
+	for(int j = 0; j < 100; ++j)
+	{
+		float in_data[in_size];
+		float out_data[out_size];
+		static_assert(in_size == out_size, "in_size != out_size");
+		for(int i = 0; i < in_size; ++i)
+		{
+			in_data[i] = urandom11();
+			out_data[out_size - i - 1] = in_data[i];
+		}
+		
+		in->getInput().write(in_data);
+		
+		for(int i = 0; i < 3; ++i)
+		{
+			//printStep(i, in, out);
+			net.stepForward();
+		}
+		
+		out->getOutput().read(in_data);
+		static_assert(in_size == out_size, "in_size != out_size");
+		for(int i = 0; i < in_size; ++i)
+		{
+			out_data[i] -= in_data[i];
+		}
+		out->getInputError().write(out_data);
+		out->getInputError().validate(true);
+		
+		for(int i = 0; i < 2; ++i)
+		{
+			//printStep(i, in, out);
+			//printConn(conn);
+			net.stepBackward();
+		}
+		
+		printConn(conn);
+		std::cout << std::endl;
+		net.commitGrad(1e-2);
+	}
 	
 	net.forConns([](Conn *conn) 
 	{
