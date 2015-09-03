4eef8654b938d820d0040fd1c1ffd231c1b0f090
Alex <alexey.gerasev@gmail.com>
2015-09-03 13:51:16 +0600

    begin hw debugging

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
 
-#include <nn/net.hpp>
+#include <nn/bp/net.hpp>
 
-#include <nn/sw/layerext.hpp>
-#include <nn/sw/conn.hpp>
+#include <nn/sw/bp/layerext.hpp>
+#include <nn/sw/bp/conn.hpp>
 
-#include <nn/hw/factory.hpp>
-#include <nn/hw/layerext.hpp>
-#include <nn/hw/conn.hpp>
+#include <nn/hw/bp/factory.hpp>
+#include <nn/hw/bp/layer.hpp>
+#include <nn/hw/bp/conn.hpp>
 
 #include "reader.hpp"
 #include "print.hpp"
@@ -19,25 +19,25 @@ int main(int argc, char *argv[])
 	
 	unsigned seed = 987654;
 	
-	FactoryHW factory("libnn/opencl/kernel.c");
+	FactoryHW_BP factory("libnn/opencl/kernel.c");
 	
-	Net net_sw, net_hw;
+	Net_BP net_sw, net_hw;
 	
-	Layer *in_sw, *in_hw;
-	Layer *out_sw, *out_hw;
+	Layer_BP *in_sw, *in_hw;
+	Layer_BP *out_sw, *out_hw;
 	
 	for(int i = 0; i < 3; ++i)
 	{
-		LayerSW *layer_sw;
-		LayerHW *layer_hw;
+		LayerSW_BP *layer_sw;
+		LayerHW_BP *layer_hw;
 		if(i != 0)
 		{
-			layer_sw = new LayerExtSW<LayerFunc::SIGMOID>(i, LAYER_SIZE[i]);
-			layer_hw = factory.newLayer(i, LAYER_SIZE[i], LayerFunc::SIGMOID);
+			layer_sw = new LayerSW_BP(i, LAYER_SIZE[i]);
+			layer_hw = factory.newLayer(i, LAYER_SIZE[i]);
 		}
 		else
 		{
-			layer_sw = new LayerSW(i, LAYER_SIZE[i]);
+			layer_sw = new LayerSW_BP(i, LAYER_SIZE[i]);
 			layer_hw = factory.newLayer(i, LAYER_SIZE[i]);
 		}
 		
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
 	srand(seed);
 	for(int i = 0; i < 2; ++i)
 	{
-		Conn *conn = new ConnSW(i, LAYER_SIZE[i], LAYER_SIZE[i + 1]);
+		Conn *conn = new ConnSW_BP(i, LAYER_SIZE[i], LAYER_SIZE[i + 1]);
 		conn->getWeight().randomize();
 		conn->getBias().randomize();
 		net_sw.addConn(conn, i, i + 1);
@@ -85,6 +85,13 @@ int main(int argc, char *argv[])
 	{
 		const float *in_data = test_set.getImages()[j].getData().data();
 		float out_data_sw[10], out_data_hw[10];
+		float result[10];
+					
+		int digit = test_set.getImages()[j].getDigit();
+		for(int i = 0; i < 10; ++i)
+		{
+			result[i] = i == digit ? 1.0f : 0.0f;
+		}
 		
 		in_sw->getInput().write(in_data);
 		in_hw->getInput().write(in_data);
@@ -103,7 +110,39 @@ int main(int argc, char *argv[])
 		{
 			diff += out_data_sw[i] - out_data_hw[i];
 		}
-		std::cout << j << " difference: " << diff << std::endl;
+		//std::cout << j << " output difference: " << diff << std::endl;
+		
+		/*
+		float max_val_sw = out_data_sw[0], max_val_hw = out_data_hw[0];
+		int max_digit_sw = 0, max_digit_hw = 0;
+		for(int i = 1; i < 10; ++i)
+		{
+			if(out_data_sw[i] > max_val_sw)
+			{
+				max_val_sw = out_data_sw[i];
+				max_digit_sw = i;
+			}
+			if(out_data_hw[i] > max_val_hw)
+			{
+				max_val_hw = out_data_hw[i];
+				max_digit_hw = i;
+			}
+		}
+		*/
+		
+		std::cout << j << " cost difference: " << out_sw->getCost(result) - out_hw->getCost(result) << std::endl;
+		
+		out_sw->setDesiredOutput(result);
+		out_hw->setDesiredOutput(result);
+		
+		for(int i = 0; i < 2; ++i)
+		{
+			net_sw.stepBackward();
+			net_hw.stepBackward();
+		}
+		
+		net_sw.commitGrad(1.0f);
+		net_hw.commitGrad(1.0f);
 	}
 	
 	net_sw.forConns([](Conn *conn)
